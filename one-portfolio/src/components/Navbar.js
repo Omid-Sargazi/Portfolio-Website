79278c77f3e6ad6e8ccd16dd60311027f1102116
Omid <om.sargazi@gmail.com>
2024-02-20 18:09:33 +0330

    build mobile navbar

--- one-portfolio/src/components/Navbar.js
+++ one-portfolio/src/components/Navbar.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
   const links = [
     {
       id: 1,
@@ -29,7 +30,7 @@ const Navbar = () => {
       <div>
         <h1 className=" text-5xl font-signature ml-2">Omid Portfolio</h1>
       </div>
-      <ul className="flex ">
+      <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
             key={id}
@@ -39,6 +40,24 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className=" md:hidden cursor-pointer pr-4 z-10 text-gray-500"
+      >
+        {nav ? <FaBars size={30} /> : <FaTimes size={30} />}
+      </div>
+      {!nav && (
+        <ul className="md:hidden flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className=" px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
